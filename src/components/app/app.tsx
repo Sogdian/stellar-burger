54a5debda794ca577db11386234840c59d0619a4
Sogdian Ashurov <sogdian4815@gmail.com>
2024-06-24 17:15:07 +0200

    добавить модалки с дополнительной информацией

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,75 +1,115 @@
 import {
-    ConstructorPage,
-    Feed,
-    ForgotPassword,
-    Login, NotFound404,
-    Profile, ProfileOrders,
-    Register,
-    ResetPassword
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
-import { Route } from 'react-router-dom';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, useNavigate } from 'react-router-dom';
 import { ProtectedRoute } from '../protected-route/protected-route';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Route path='/' element={<ConstructorPage />} />
-    <Route path='/feed' element={<Feed />} />
-    <Route
-      path='/login'
-      element={
-        <ProtectedRoute onlyUnAuth>
-          <Login />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path='/register'
-      element={
-        <ProtectedRoute onlyUnAuth>
-          <Register />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path='/forgot-password'
-      element={
-        <ProtectedRoute onlyUnAuth>
-          <ForgotPassword />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path='/reset-password'
-      element={
-        <ProtectedRoute onlyUnAuth>
-          <ResetPassword />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path='/profile'
-      element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      }
-    />
-    <Route
-      path='/profile/orders'
-      element={
-        <ProtectedRoute>
-          <ProfileOrders />
-        </ProtectedRoute>
-      }
-    />
-    <Route path='*' element={<NotFound404 />} />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRoute onlyUnAuth>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRoute onlyUnAuth>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRoute onlyUnAuth>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRoute onlyUnAuth>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal
+            title='Заказ'
+            children={<OrderInfo />}
+            onClose={() => navigate('/feed')}
+          />
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal
+            title='Детали ингредиента'
+            children={<IngredientDetails />}
+            onClose={() => navigate('/')}
+          />
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <Modal
+            title='Заказ'
+            children={
+              <ProtectedRoute>
+                <OrderInfo />
+              </ProtectedRoute>
+            }
+            onClose={() => navigate('/profile/orders')}
+          />
+        }
+      />
+      <ConstructorPage />
+    </div>
+  );
+};
 
 export default App;
