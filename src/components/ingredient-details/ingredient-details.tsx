da74d91f22790ebd936b8f0bf64d24beb078118d
Sogdian Ashurov <sogdian4815@gmail.com>
2024-06-26 16:43:39 +0200

    добаил IngredientDetails

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,14 +1,33 @@
 import { FC } from 'react';
-import { Preloader } from '../ui/preloader';
-import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { Preloader, IngredientDetailsUI } from '@ui';
+import { useSelector } from '../../services/store';
+import {
+  getIngredientsSelector,
+  getLoadingIngredientsSelector
+} from '../../services/ingredientsSlice';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const ingredients = useSelector(getIngredientsSelector);
+  const ingredientData = ingredients.find(
+    (element) => element._id === params.id!
+  );
+
+  const params = useParams();
+  const isIngredientsLoading = useSelector(getLoadingIngredientsSelector);
 
   if (!ingredientData) {
     return <Preloader />;
   }
 
-  return <IngredientDetailsUI ingredientData={ingredientData} />;
+  return (
+    <>
+      {isIngredientsLoading ? (
+        <Preloader />
+      ) : (
+        <IngredientDetailsUI ingredientData={ingredientData} />
+      )}
+    </>
+  );
 };
