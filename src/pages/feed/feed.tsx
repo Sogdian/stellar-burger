7a683bd0811c2382fcd05f8d64aa76429ab519fa
Sogdian Ashurov <sogdian4815@gmail.com>
2024-06-26 17:22:50 +0200

    поправил Login и Feed

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,30 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { getFeed, getOrdersSelector } from '../../services/feedSlice';
 
 export const Feed: FC = () => {
   /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const orders: TOrder[] = useSelector(getOrdersSelector);
 
   if (!orders.length) {
     return <Preloader />;
   }
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getFeed());
+  }, [dispatch]);
+
+  const handleGetFeed = useCallback(() => {
+    dispatch(getFeed());
+  }, [dispatch]);
+
+  return (
+    <>
+      <FeedUI orders={orders} handleGetFeeds={handleGetFeed} />
+    </>
+  );
 };
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,28 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import {
+  getIsAuthCheckedSelector,
+  loginUser
+} from '../../services/userSlice';
+import { Navigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(getIsAuthCheckedSelector);
+  if (isAuthenticated) {
+    return <Navigate to={'/'} />;
+  }
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    if (!email || !password) {
+      return;
+    }
+    dispatch(loginUser({ email, password }));
   };
 
   return (
