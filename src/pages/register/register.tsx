284d3666a9d2f66693d7bcd98bbdf618b2bedbde
Sogdian Ashurov <sogdian4815@gmail.com>
2024-06-26 18:54:30 +0200

    поправил Register, ProfileOrders

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,14 +1,39 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import {
+  getIsAuthCheckedSelector,
+  registerUser
+} from '../../services/userSlice';
+import { Navigate } from 'react-router-dom';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const isAuthenticated = useSelector(getIsAuthCheckedSelector);
+
+  const dispatch = useDispatch();
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    if (!userName || !email) {
+      return;
+    }
+    dispatch(
+      registerUser({
+        name: userName,
+        email,
+        password
+      })
+    );
   };
+
+  if (isAuthenticated) {
+    return <Navigate to={'/'} />;
+  }
 
   return (
     <RegisterUI
