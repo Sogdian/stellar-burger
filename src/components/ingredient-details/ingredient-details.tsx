7228f410392c106155374d3d1a8109fa5bf931c9
Sogdian Ashurov <sogdian4815@gmail.com>
2024-06-27 22:15:18 +0200

    фикс открытия ингридиента

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -10,11 +10,11 @@ import { useParams } from 'react-router-dom';
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
   const ingredients = useSelector(getIngredientsSelector);
+  const params = useParams();
   const ingredientData = ingredients.find(
     (element) => element._id === params.id!
   );
 
-  const params = useParams();
   const isIngredientsLoading = useSelector(getLoadingSelector);
 
   if (!ingredientData) {
