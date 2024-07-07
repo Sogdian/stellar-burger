abd4baa1d7f3fb27f60710baf8f0dd307e74de65
Sogdian Ashurov <sogdian4815@gmail.com>
2024-07-07 17:32:00 +0200

    loginUser

--- src/services/tests/userSlice.test.ts
+++ src/services/tests/userSlice.test.ts
@@ -1,11 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { getUser } from '../userSlice';
+import { getUser, loginUser } from '../userSlice';
 import { rootReducer } from '../store';
 
 const MAIL = 'mail@fortest.com';
 const USER = 'testUser';
+const PASSWORD = 'test';
+
 jest.mock('@api', () => ({
   getUserApi: jest.fn(() =>
+    Promise.resolve({
+      user: {
+        email: MAIL,
+        name: USER
+      }
+    })
+  ),
+  loginUserApi: jest.fn(() =>
     Promise.resolve({
       user: {
         email: MAIL,
@@ -21,6 +31,30 @@ describe('userSlice', () => {
       reducer: { user: rootReducer }
     });
     await store.dispatch(getUser());
+
+    const state = store.getState().user;
+    const expected = state.user.user;
+
+    const actual = {
+      email: MAIL,
+      name: USER
+    };
+
+    expect(expected).toEqual(actual);
+    expect(state.user.isAuthChecked).toBeTruthy();
+    expect(state.user.isAuthenticated).toBeTruthy();
+  });
+
+  test('loginUser', async () => {
+    const store = configureStore({
+      reducer: { user: rootReducer }
+    });
+    await store.dispatch(
+      loginUser({
+        email: MAIL,
+        password: PASSWORD
+      })
+    );
 
     const state = store.getState().user;
     const expected = state.user.user;
